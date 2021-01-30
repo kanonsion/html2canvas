3bd3f6435c8294e79b14bae233017fe89490f481
kanonsion <44792662+kanonsion@users.noreply.github.com>
2021-01-30 15:53:50 +0800

    Update canvas-renderer.ts

--- src/render/canvas/canvas-renderer.ts
+++ src/render/canvas/canvas-renderer.ts
@@ -583,10 +583,8 @@ export class CanvasRenderer {
 
                 ctx.fillStyle = gradient;
                 ctx.fillRect(0, 0, width, height);
-                if (width > 0 && height > 0) {
-                    const pattern = this.ctx.createPattern(canvas, 'repeat') as CanvasPattern;
-                    this.renderRepeat(path, pattern, x, y);
-                }
+                const pattern = this.ctx.createPattern(canvas, 'repeat') as CanvasPattern;
+                this.renderRepeat(path, pattern, x, y);
             } else if (isRadialGradient(backgroundImage)) {
                 const [path, left, top, width, height] = calculateBackgroundRendering(container, index, [
                     null,
